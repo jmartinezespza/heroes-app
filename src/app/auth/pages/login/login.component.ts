27e4ffe8143b87833e874a2c8a5c720277ef20be
Jhonny Martínez Espinoza <jmartinezespza@gmail.com>
2021-07-30 13:56:04 -0500

    Autenticacion y bloqueo de urls

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router:Router,
+                private authService: AuthService ) { }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    // ir al backend
+    // un usuario
+
+    this.authService.login()
+      .subscribe( resp => {
+
+        if ( resp.id ) {
+          this.router.navigate(['./heroes']);
+        }
+        
+      } );
+
+    // this.router.navigate(['./heroes']);
   }
 
 }
